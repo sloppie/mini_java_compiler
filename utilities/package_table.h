a926b9837cffcdd94bb802c97a79207508e439df
sloppie <dgitau75@gmail.com>
2020-04-16 23:45:56 -0400

    Added comments for the PackageTable class

--- utilities/package_table.h
+++ utilities/package_table.h
@@ -6,6 +6,9 @@
 
 using namespace std;
 
+// This class is extremely similar to the SymbolTable class
+// refer to the comments made on the @SymbolTable class for more information on
+// how they work
 class PackageTable {
     public:
         PackageTable();
