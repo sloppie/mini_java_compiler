d64de642e74e3acc0a2e95da7d3ef39adbba4e84
sloppie <dgitau75@gmail.com>
2020-04-16 23:58:22 -0400

    Added comments to the Node class

--- utilities/data_structures/node.h
+++ utilities/data_structures/node.h
@@ -6,6 +6,14 @@
 
 using namespace std;
 
+// This data structure is the basic mode of communication between all the parts of the compiler.
+// When the lexer identifies a valid token, it creates a Node which contains information such:
+// @param1 whether it is a terminal or not (bool)
+// @param2 name of the node, this names are based on the names given in the CFG class
+// if the Node is supposed to be the end of the parsing of the tree, the Node is given a value:
+// @param3 (second constructor overload) hold the value of the Node.
+// in most cases the Node is a Non-terminal unless it's a comparator Node which is a terminal Node,
+// but requires all three parameters filled. (this is because these terinals are four different ones). 
 class Node {
 
     public:
